2262181e5798a1c3bb834868d7c505c09d0fd57c
Bakhrom <115355471+Bakhromtdiu@users.noreply.github.com>
2023-05-30 22:23:57 -0500

    Create Prog1.java

---
+++ Prog1.java
@@ -0,0 +1,11 @@
+public class Prog1 {
+    public static void main(String[] args) {
+        int x = RandomNumbers.getRandomInt(1, 9);
+        double piPowerX = Math.pow(Math.PI, x);
+        System.out.println("π^" + x + " = " + piPowerX);
+
+        int y = RandomNumbers.getRandomInt(3, 14);
+        double yPowerPi = Math.pow(y, Math.PI);
+        System.out.println(y + "^π = " + yPowerPi);
+    }
+}
