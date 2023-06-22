6d53ea37365010f9692d30e64fbc0aa219a0575e
Bakhrom <115355471+Bakhromtdiu@users.noreply.github.com>
2023-06-22 14:43:45 -0500

    Update prob2.java

--- assaigment12/prob2.java
+++ assaigment12/prob2.java
@@ -1,1 +1,29 @@
 
+public class CustomerAccount {
+
+    private String Cus_name;
+    private double Acc_No;
+    private double Balance;
+
+    public boolean deposit(double amount) {
+        if (amount < 0)
+            return false;
+        Balance += amount;
+        return true;
+    }
+
+    public boolean withdraw(double amount) throws Exception {
+        if (Balance < 100) {
+            return false;
+        }
+
+        if (amount > Balance)
+            throw new InsufficientBalanceException("amount exceeds balance");
+        Balance -= amount;
+        return true;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+}
