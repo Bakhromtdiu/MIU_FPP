5c023a935e9d46e453aa8430dae5f165391b31a7
Bakhrom <115355471+Bakhromtdiu@users.noreply.github.com>
2023-06-23 11:41:58 -0500

    Update BinarySearchTree.java

--- assaignment10/BinarySearchTree.java
+++ assaignment10/BinarySearchTree.java
@@ -1,3 +1,29 @@
 PRE ORDER : 0050 , 0010 , 0008 , 0005,0009,0003,0026,0031,0150,0100,0079,0126,0152,0151,0180
 IN ORDER : 0005,0008,0009,0010,0026,0030,0031,0050,0079,0100,0126,0150,0151,0180
 Post Order : 0005,0009,0008,0026,0031,0030,0010,0079,0126,0100,0151,0180,0152,0150,0050 
+
+
+Drawing Runstack inOrder(0006)
+  InOtderTraversal(null)
+  console(0010)
+  InOtderTraversal(nul)
+InOtderTraversal(0010)
+  console(0009)
+  InOtderTraversal(null)
+  Console(0006)
+InOtderTraversal(null)
+InOtderTraversal(0006)
+  InOtderTraversal(0009)
+console(0006)
+InOtderTraversal(0006)
+  console(0004)
+InOtderTraversal(null)
+InOtderTraversal(0004)
+Console(0003)
+InOtderTraversal(null)
+InOtderTraversal(0003)
+inOrderTraversal(null)  
+Console shows (0001)
+inOrderTraversal(null)
+inOrdertraversal(0001)
+inOrderTraversal(0003)
